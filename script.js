9f6b179873e784e8fd615db7501e9b6eecf22fb0
sharkonanar <carrygerard01@gmail.com>
2025-12-29 17:39:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ window.onload = function() {
     const grille = document.getElementById('video-grid');
     
     if (typeof videos !== 'undefined') {
-        grille.innerHTML = ""; // On vide la grille
+        grille.innerHTML = ""; 
 
         videos.forEach(film => {
-            // Ici, on utilise EXACTEMENT la structure simple 
-            // qui marche avec ton fichier style.css
             grille.innerHTML += `
-                <div class="video">
+                <div class="video-card">
                     <a href="video.html?id=${film.id}">
                         <img src="${film.image}" alt="${film.titre}">
-                        <h3>${film.titre}</h3>
+                        <div class="video-info">
+                            <h3>${film.titre}</h3>
+                            <p>${film.resume}</p>
+                        </div>
                     </a>
                 </div>
             `;
