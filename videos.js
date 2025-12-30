278b4155523e6c62110f8326893509c744bd36af
sharkonanar <carrygerard01@gmail.com>
2025-12-30 16:34:16 +0100

    Update videos.js

--- videos.js
+++ videos.js
@@ -5,12 +5,14 @@ const videos = [
         resume: "Une épopée incroyable avec des motards aux looks SM et des flics pas gentils qu' ils sont méchants.",
         image: "images/guerriersbronx.jpg",
         videoUrl: "https://www.youtube.com/embed/cCzi0HWhras",
-        note: 4 // <--- Ajoute la note ici (de 1 à 5)
+        note: 4
     },
     {
         id: "2",
         titre: "L'Attaque de la Tomate",
         resume: "Un film culte où des légumes prennent le pouvoir.",
-        videoUrl: "https://www.youtube.com/embed/dQw4w9WgXcQ"
+        image: "images/tomate.jpg",
+        videoUrl: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+        note: 3
     }
 ];
